4aa9ab13541339afe8b41d9ad06819e3a39a4de7
sandeepmaurya6 <sandeep886626@gmail.com>
2024-12-09 23:49:51 +0530

    added api error handler utils

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ const app = express();
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("MongoDB connection failed.", error);
+});
 
 /*
 //approach 1 to connect db
